fe23d6c41547f5f68ef22829511228bb2e08cf69
takuhisa <iamtakuhisam@gmail.com>
2023-03-09 17:00:50 +0900

    day5

--- day5.py
+++ day5.py
@@ -21,3 +21,13 @@ st.write(df)
 # Example 4
 
 st.write("Below is a DataFrame:", df, "Above is a dataframe")
+
+# Example 5
+
+df2 = pd.DataFrame(np.random.randn(200, 30), columns=["a", "b", "c"])
+c = (
+    alt.Chart(df2)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+st.write(c)
