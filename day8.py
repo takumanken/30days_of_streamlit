59a651398d5409f6e95027e712bbaa9441ada20b
takuhisa <iamtakuhisam@gmail.com>
2023-03-09 17:12:25 +0900

    day8

---
+++ day8.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from datetime import time, datetime
+
+st.header('st.slider')
+
+# Example 1
+
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm", age, 'years old')
