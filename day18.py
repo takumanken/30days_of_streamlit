31c516df9ae8ae12c8ee880db8a50f96c1c03843
takuhisa <iamtakuhisam@gmail.com>
2023-03-09 14:35:55 +0000

    day19

--- day18.py
+++ day18.py
@@ -2,3 +2,14 @@ import streamlit as st
 import pandas as pd
 
 st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.subheader(uploaded_file)
+    st.write(df)
+    st.subheader(df.describe())
+else:
+    st.info("Upload a CSV file")
